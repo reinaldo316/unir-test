8a420b7613ccd0400cb24f36a74c691eac0fe508
Edgar Guerra <reinaldo316@gmail.com>
2023-05-15 10:21:52 -0500

    se comenta el codigo

--- app/api.py
+++ app/api.py
@@ -1,22 +1,25 @@
-import http.client
-import math
+import http.client  # Módulo para realizar operaciones HTTP
+import math  # Módulo para realizar operaciones matemáticas
 
+from flask import Flask  # Importa la clase Flask del framework Flask
 
-from flask import Flask
+from app import util  # Importa el módulo util desde el paquete app
+from app.calc import Calculator  # Importa la clase Calculator desde el módulo calc del paquete app
 
-from app import util
-from app.calc import Calculator
+CALCULATOR = Calculator()  # Instancia un objeto de la clase Calculator
+api_application = Flask(__name__)  # Crea una instancia de la aplicación Flask
+HEADERS = {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"}  # Encabezados de la respuesta HTTP
 
-CALCULATOR = Calculator()
-api_application = Flask(__name__)
-HEADERS = {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"}
-
-
+# Ruta principal
 @api_application.route("/")
 def hello():
     return "Hello from The Calculator!\n"
 
 
+#En todas las rutas se realiza la conversión del parámetro num a un número utilizando el método convert_to_number del módulo util. 
+#En todas las rutas si ocurre algún error de tipo (TypeError), se captura y se devuelve una respuesta de error con el código HTTP 400 (BAD_REQUEST).
+
+# Ruta para la operación de suma
 @api_application.route("/calc/add/<op_1>/<op_2>", methods=["GET"])
 def add(op_1, op_2):
     try:
@@ -25,7 +28,7 @@ def add(op_1, op_2):
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
-
+# Ruta para la operación de resta
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
 def substract(op_1, op_2):
     try:
@@ -34,6 +37,7 @@ def substract(op_1, op_2):
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
+# Ruta para la operación de multiplicación
 @api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
 def multiply(op_1, op_2):
     try:
@@ -42,7 +46,7 @@ def multiply(op_1, op_2):
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
-
+# Ruta para la operación de división
 @api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
 def divide(op_1, op_2):
     try:
@@ -51,7 +55,7 @@ def divide(op_1, op_2):
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
-
+# Ruta para la operación de potencia
 @api_application.route("/calc/power/<base>/<exponent>", methods=["GET"])
 def power(base, exponent):
     try:
@@ -60,7 +64,7 @@ def power(base, exponent):
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
-
+# Ruta para la operación de raíz cuadrada
 @api_application.route("/calc/square_root/<num>", methods=["GET"])
 def square_root(num):
     try:
@@ -69,7 +73,7 @@ def square_root(num):
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
-
+# Ruta para la operación de logaritmo
 @api_application.route("/calc/logarithm/<num>", methods=["GET"])
 def logarithm(num):
     try:
@@ -77,3 +81,4 @@ def logarithm(num):
         return ("{}".format(math.log10(num)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
--- app/calc.py
+++ app/calc.py
@@ -1,59 +1,57 @@
-import app
+import app  # Importa el módulo app
 
-
-class InvalidPermissions(Exception):
+# Clase de excepción que se utiliza para representar errores relacionados con permisos no válidos.
+   class InvalidPermissions(Exception):
     pass
-
 
 class Calculator:
     def add(self, x, y):
-        self.check_types(x, y)
+        self.check_types(x, y)  # Verifica los tipos de x e y
         return x + y
 
     def substract(self, x, y):
-        self.check_types(x, y)
+        self.check_types(x, y)  # Verifica los tipos de x e y
         return x - y
 
     def multiply(self, x, y):
-        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
-            raise InvalidPermissions('User has no permissions')
+        if not app.util.validate_permissions(f"{x} * {y}", "user1"):  # Verifica los permisos utilizando una función de app.util
+            raise InvalidPermissions('User has no permissions')  # Lanza una excepción de InvalidPermissions si no se tienen permisos suficientes
 
-        self.check_types(x, y)
+        self.check_types(x, y)  # Verifica los tipos de x e y
         return x * y
 
     def divide(self, x, y):
-        self.check_types(x, y)
+        self.check_types(x, y)  # Verifica los tipos de x e y
         if y == 0:
-            raise TypeError("Division by zero is not possible")
+            raise TypeError("Division by zero is not possible")  # Lanza una excepción de TypeError si el denominador es cero
 
         return x / y
 
     def power(self, x, y):
-        self.check_types(x, y)
+        self.check_types(x, y)  # Verifica los tipos de x e y
         return x ** y
 
     def square_root(self, x):
-        self.check_type(x)
+        self.check_type(x)  # Verifica el tipo de x
         if x < 0:
-            raise ValueError("Square root of a negative number is not possible")
+            raise TypeError("Square root of a negative number is not possible")  # Lanza una excepción de TypeError si el número es negativo
         return x ** 0.5
 
     def logarithm(self, x):
-        self.check_type(x)
+        self.check_type(x)  # Verifica el tipo de x
         if x <= 0:
-            raise ValueError("Logarithm of a non-positive number is not possible")
+            raise TypeError("Logarithm of a non-positive number is not possible")  # Lanza una excepción de TypeError si el número es no positivo
         return math.log10(x)
 
     def check_types(self, x, y):
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Parameters must be numbers")
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):  # Verifica si x e y son instancias de int o float
+            raise TypeError("Parameters must be numbers")  # Lanza una excepción de TypeError si los parámetros no son números
 
     def check_type(self, x):
-        if not isinstance(x, (int, float)):
-            raise TypeError("Parameter must be a number")
-
+        if not isinstance(x, (int, float)):  # Verifica si x es una instancia de int o float
+            raise TypeError("Parameter must be a number")  # Lanza una excepción de TypeError si el parámetro no es un número
 
 if __name__ == "__main__":  # pragma: no cover
-    calc = Calculator()
-    result = calc.add(2, 2)
-    print(result)
+    calc = Calculator()  # Crea una instancia de la clase Calculator
+    result = calc.add(2, 2)  # Llama al método add de la instancia calc con los argumentos 2 y 2
+    print(result)  # Imprime el resultado de la suma
