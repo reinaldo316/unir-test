12d554f7157b099b4105c599df6e156f3cc7b08e
Edgar Guerra <reinaldo316@gmail.com>
2023-05-15 11:25:47 -0500

    se completa los test de api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -1,23 +1,75 @@
-import http.client
-import os
-import unittest
-from urllib.request import urlopen
+import http.client  # Importa el módulo http.client para realizar solicitudes HTTP
+import os  # Importa el módulo os para acceder a variables de entorno y funcionalidades del sistema operativo
+import unittest  # Importa el módulo unittest para pruebas unitarias
+from urllib.request import urlopen  # Importa la función urlopen del módulo urllib.request para  solicitudes HTTP
 
-import pytest
+import pytest  # Importa el módulo pytest para extender las capacidades de prueba
 
-BASE_URL = os.environ.get("BASE_URL")
-DEFAULT_TIMEOUT = 2  # in secs
+BASE_URL = os.environ.get("BASE_URL")  # Obtiene el valor de la variable de entorno BASE_URL
+DEFAULT_TIMEOUT = 2  # Define el tiempo de espera 
 
-
-@pytest.mark.api
+@pytest.mark.api  
 class TestApi(unittest.TestCase):
     def setUp(self):
-        self.assertIsNotNone(BASE_URL, "URL no configurada")
-        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+        # Configura el entorno de prueba antes de cada método de prueba
+        self.assertIsNotNone(BASE_URL, "URL no configurada")  # Verifica que BASE_URL no sea None
+        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")  # Verifica que BASE_URL tenga una longitud mayor a 8
 
     def test_api_add(self):
-        url = f"{BASE_URL}/calc/add/2/2"
+        # Prueba el endpoint "/calc/add" de la API
+        url = f"{BASE_URL}/calc/add/2/2"  # Construye la URL para la solicitud
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)  # Realiza una solicitud HTTP a la URL
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )  # Verifica que el estado de la respuesta sea 200 (OK)
+
+    def test_api_substract(self):
+        # Prueba el endpoint "/calc/substract" de la API
+        url = f"{BASE_URL}/calc/substract/5/3"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
+
+    def test_api_multiply(self):
+        # Prueba el endpoint "/calc/multiply" de la API
+        url = f"{BASE_URL}/calc/multiply/4/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_divide(self):
+        # Prueba el endpoint "/calc/divide" de la API
+        url = f"{BASE_URL}/calc/divide/10/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_power(self):
+        # Prueba el endpoint "/calc/power" de la API
+        url = f"{BASE_URL}/calc/power/2/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_square_root(self):
+        # Prueba el endpoint "/calc/square_root" de la API
+        url = f"{BASE_URL}/calc/square_root/16"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_logarithm(self):
+        # Prueba el endpoint "/calc/logarithm" de la API
+        url = f"{BASE_URL}/calc/logarithm/100"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    if __name__ == "__main__":
+        unittest.main()    
